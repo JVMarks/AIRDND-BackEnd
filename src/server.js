98d8d6e3732702d13889c8166f6dd2fa1629bb4f
João Victor Marks <jovic.marks@gmail.com>
2020-03-08 22:16:24 -0300

    AIRDNDBACKEND

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require ('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-apskq.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+   const { user_id } = socket.handshake.query;
+
+   connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+})
+
+//SAO METADOS PARA app. GET, POST, PUT ,DELETE
+
+//req.query = acessar query params (para filtros)
+//req.params = acessar route params (para edição, delete)
+//req.body =  acessar corpo da requisição (para criação , edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
+app.use(routes);
+
+server.listen(1119);
+
+//yarn init -y para baixar o pacote
+// yarn add nodemon -D
+//yarn dev para iniciar o servidor
+// banco de dados yarn add mongoose
+//^C fechar servidors
+//yarn add multer
+//yarn add socket.io
