531d41927e7c8229fff7e37ba4b0c9508e37a848
João Victor Marks <jovic.marks@gmail.com>
2020-03-10 20:18:48 -0300

    ADD2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const path = require ('path');
 
 const socketio = require('socket.io');
@@ -42,6 +42,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
+
+app.use(express.static(__dirname + '/public'));
 
 server.listen(process.env.PORT || 1119);
 
