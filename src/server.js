fd3f505ab2b217550540ad7ca69565e3a810ee6d
João Victor Marks <jovic.marks@gmail.com>
2020-03-12 17:50:08 -0300

    A

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
 const path = require ('path');
 
 const socketio = require('socket.io');
@@ -11,6 +12,8 @@ const routes = require('./routes');
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-apskq.mongodb.net/test?retryWrites=true&w=majority',{
   useNewUrlParser: true,
@@ -29,6 +32,8 @@ app.use((req, res, next) => {
   req.io = io;
   req.connectedUsers = connectedUsers;
 
+
+
   return next();
 })
 
@@ -42,6 +47,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
+
 
 server.listen(process.env.PORT || 1119);
 
