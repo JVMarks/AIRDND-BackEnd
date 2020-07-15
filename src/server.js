4abb722ae919602f18a2eb7e9cf712b78cd7263d
JVMarks <jovic.marks@gmail.com>
2020-07-15 17:37:37 -0300

    v2.0

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
+const mongoose = require('mongoose');
+const favicon = require('serve-favicon');
+const path = require ('path');
 
 const socketio = require('socket.io');
 const http = require('http');
@@ -11,10 +12,10 @@ const routes = require('./routes');
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 
-mongoose.connect(
-  'mongodb+srv://omnistack:omnistack@cluster0-apskq.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-apskq.mongodb.net/test?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -22,14 +23,16 @@ mongoose.connect(
 const connectedUsers = {};
 
 io.on('connection', socket => {
-  const { user_id } = socket.handshake.query;
+   const { user_id } = socket.handshake.query;
 
-  connectedUsers[user_id] = socket.id;
+   connectedUsers[user_id] = socket.id;
 });
 
 app.use((req, res, next) => {
   req.io = io;
   req.connectedUsers = connectedUsers;
+
+
 
   return next();
 })
@@ -42,8 +45,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
 
 
